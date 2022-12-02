d0e8a719e3ec0ee93c87cea6bff373edb31cd4d3
Eloy Eligon <ee.eligon@gmail.com>
2022-12-02 08:27:53 -0300

    fix: use packages in setuptools

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 setup(
     name='dbt-mermaid',
     version='0.1.0',
-    py_modules=['cli'],
+    packages=find_packages(),
+    include_package_data=True,
     install_requires=[
         'Click',
     ],
